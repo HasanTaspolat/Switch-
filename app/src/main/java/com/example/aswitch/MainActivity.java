5561eb8caca6b5e34f12daf37154f7697a67b178
OsmanYavuzOlgun <osmanyavuzolgun@gmail.com>
2021-11-17 17:11:08 +0300

    Switch :: LogoAdjustment
    Logo Adjustment for ImageView starter page.

--- app/src/main/java/com/example/aswitch/MainActivity.java
+++ app/src/main/java/com/example/aswitch/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.aswitch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
+        logo = findViewById(R.id.imageView3) ;
+
+        logo.setImageResource(R.drawable.logo);
+
     }
+
+
 
    
 }
