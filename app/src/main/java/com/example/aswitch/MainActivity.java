a9a07c32163e1765b454cd58902399cee841bef9
OsmanYavuzOlgun <osmanyavuzolgun@gmail.com>
2021-11-17 13:54:35 +0300

    Switch :: Comment_Try:Try

--- app/src/main/java/com/example/aswitch/MainActivity.java
+++ app/src/main/java/com/example/aswitch/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     public class Dolar{
         public float dolarVal;
         public Dolar() {
+            //hello git
         }
     }
 }
