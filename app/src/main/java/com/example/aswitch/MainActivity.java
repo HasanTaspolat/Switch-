4366e5abb01c79bbdc4ef7c7608ca56ab6b21e26
h-taspolat.jotform.dev <65667577+HasanTaspolat@users.noreply.github.com>
2021-12-09 17:06:38 +0000

    Switch :: Register and Login dialog modals

--- app/src/main/java/com/example/aswitch/MainActivity.java
+++ app/src/main/java/com/example/aswitch/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.aswitch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView logo;
-
+    Button register,btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +34,55 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void onClick(View view) {
 
+        if(view.getId() == R.id.Login){
+        displayDialog();
+        }
+        else if(view.getId() == R.id.register){
+displayDialogRegister();
+        }
 
-   
+    }
+    public void displayDialog(){
+        final TextView tv;
+        EditText email, surname;
+        Button btnlogin;
+
+        Dialog customDialog = new Dialog(this);
+
+        customDialog.setContentView(R.layout.dialog);
+        tv =  customDialog.findViewById(R.id.logintext);
+        btnlogin = customDialog.findViewById(R.id.btnClose);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customDialog.dismiss();
+            }
+        });
+        customDialog.show();
+    }
+
+        public void displayDialogRegister(){
+        final TextView tv;
+        EditText email, surname;
+        Button btnlogin;
+
+        Dialog customDialog = new Dialog(this);
+
+        customDialog.setContentView(R.layout.diaolog_register);
+        tv =  customDialog.findViewById(R.id.logintext);
+        btnlogin = customDialog.findViewById(R.id.btnClose);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customDialog.dismiss();
+            }
+        });
+        customDialog.show();
+    }
 }
+
+
